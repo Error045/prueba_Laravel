708fa43637df3643c3075a49b23ff8186963b048
Error045 <gabriel.jamett@gmail.com>
2025-02-23 17:24:22 -0300

    Segundus

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
-     *
+     *HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
      * @return void
      */
     public function __construct()
